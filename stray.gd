26d6f21d820a719f0441882d78f77259749861d5
jooj :] <jaz@joojs-laptop.local>
2025-08-01 14:16:16 +0400

    completed walk cycle and added code for movement

---
+++ stray.gd
@@ -0,0 +1,28 @@
+extends Area2D
+@export var speed = 300
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.play("idle")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	# no wandering outside the screen. hehehehe >:)
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x > 0
