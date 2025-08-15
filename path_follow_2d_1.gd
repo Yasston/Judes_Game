9b93040624c8db28107b804139dd77c4ca369a8f
jooj :] <jaz@joojs-laptop.local>
2025-08-15 10:41:35 +0400

    added bg and obbjects folders tobranch

---
+++ path_follow_2d_1.gd
@@ -0,0 +1,10 @@
+extends PathFollow2D
+@export var speed = 400
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
